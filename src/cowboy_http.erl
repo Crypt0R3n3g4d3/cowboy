01f7b0a8fcbd4ecec1fec088aa517f5f1bf27cd2
Robert J. Macomber <robert.macomber@socrata.com>
2021-02-09 11:03:16 -0800

    Rework to make the reset-idle-on-send behavior optional

--- src/cowboy_http.erl
+++ src/cowboy_http.erl
@@ -29,6 +29,7 @@
 	env => cowboy_middleware:env(),
 	http10_keepalive => boolean(),
 	idle_timeout => timeout(),
+	reset_idle_on_send => boolean(),
 	inactivity_timeout => timeout(),
 	initial_stream_flow_size => non_neg_integer(),
 	linger_timeout => timeout(),
@@ -1080,7 +1081,7 @@ commands(State0=#state{socket=Socket, transport=Transport,
 	commands(State, StreamID, Tail);
 %% Send a response body chunk.
 %% @todo We need to kill the stream if it tries to send data before headers.
-commands(State0=#state{socket=Socket, transport=Transport, streams=Streams0, out_state=OutState},
+commands(State0=#state{socket=Socket, transport=Transport, streams=Streams0, out_state=OutState, opts=Opts},
 		StreamID, [{data, IsFin, Data}|Tail]) ->
 	%% Do not send anything when the user asks to send an empty
 	%% data frame, as that would break the protocol.
@@ -1131,12 +1132,18 @@ commands(State0=#state{socket=Socket, transport=Transport, streams=Streams0, out
 			end,
 			Stream0#stream{local_sent_size=SentSize}
 	end,
-	State = case IsFin of
+	State1 = case IsFin of
 		fin -> State0#state{out_state=done};
 		nofin -> State0
 	end,
+	State = case maps:get(reset_idle_on_send, Opts, false) of
+		true ->
+			set_timeout(State1, idle_timeout);
+		false ->
+			State1
+	end,
 	Streams = lists:keyreplace(StreamID, #stream.id, Streams0, Stream),
-	commands(set_timeout(State#state{streams=Streams}, idle_timeout), StreamID, Tail);
+	commands(State#state{streams=Streams}, StreamID, Tail);
 commands(State=#state{socket=Socket, transport=Transport, streams=Streams, out_state=OutState},
 		StreamID, [{trailers, Trailers}|Tail]) ->
 	case stream_te(OutState, lists:keyfind(StreamID, #stream.id, Streams)) of
